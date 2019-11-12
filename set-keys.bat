41575e4917de2b3094bb5b91431679970693bb72
Bob Tanner <tanner@real-time.com>
2019-11-11 21:03:36 -0600

    Clean up

--- set-keys.bat
+++
@@ -1,4 +0,0 @@
-choco apikey --key d9abae49-4ced-4e5f-9bb3-ab00fae853ea --source https://push.chocolatey.org/ 
-
-choco apikey --key 93644667-55AF-402B-D46F-8DEE8761E22A --source  http://chocolatey.int.celadonsystems.com/upload    
-
