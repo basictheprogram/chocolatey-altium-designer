45d807afb97f849b6905644fc2c9ee8188d92c41
Bob Tanner <tanner@real-time.com>
2020-02-26 12:05:51 -0600

    New version 19 released Altium Designer 19.1.9

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,17 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 
-# https://s3.amazonaws.com/altium-release-manager/Altium_Designer_19/OfflineSetup_Altium_Designer_Public_19_1_8.zip
-# altium_designer_offline_installer_19_1_8.zip
-# https://s3.amazonaws.com/altium-release-manager/Altium_Designer_19/OfflineSetup_Altium_Designer_Public_19_1_8.zip?response-content-disposition=attachment;%20filename=%22altium_designer_offline_installer_19_1_8.zip%22&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAI5TSRXSMZE33ALGA/20191017/us-east-1/s3/aws4_request&X-Amz-Date=20191017T175959Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Signature=d1524ba989315041d071f1d2885a046a23fd59a638ad050ed44dbe5750c9ae67
-
+# https://s3.amazonaws.com/altium-release-manager/Altium_Designer_19/OfflineSetup_Altium_Designer_Public_19_1_9.zip
+#
 $packageName = $env:ChocolateyPackageName
-#$fullPackage = "AltiumDesignerSetup_19_1_8.exe"
-#$url64 = 'https://s3.amazonaws.com/altium-release-manager/Altium_Designer_19/' + $fullPackage
-#$checksum64 = '0b3b206090b4c6b51544090404487901dc9af7ed7b9322835ccfe76c6eaf7c61'
-
-$fullPackage = "OfflineSetup_Altium_Designer_Public_19_1_8.zip"
+$fullPackage = "OfflineSetup_Altium_Designer_Public_19_1_9.zip"
 $url64 = 'https://s3.amazonaws.com/altium-release-manager/Altium_Designer_19/' + $fullPackage
-$checksum64 = 'a6f4bb27539d0f28d946b0b9c79351f93925036ee304c9c0b794a3e6a406fd19'
+$checksum64 = 'd2e98bfa4acc936e6bd5b19bd7608500c00357d8d205540e5f55c8ef2fedb564'
 
 $WorkSpace = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
